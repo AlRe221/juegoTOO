9df06639578ac98bfbee6f0d61cecb5987ba31ee
AlRe221 <rilex2205@gmail.com>
2025-05-03 09:53:00 -0600

    se agrego que los objetos no aparezcan en pixeles con colisión

--- src/Main/AssetSetter.java
+++ src/Main/AssetSetter.java
@@ -46,10 +46,17 @@ public class AssetSetter {
 			}
 
 			// posición aleatoria
-			int x = rand.nextInt(50); // suponiendo 50 tiles en X
-			int y = rand.nextInt(30); // suponiendo 30 tiles en Y
-			gP.o[i].setWorldX(x * gP.getTamanioTile());
-			gP.o[i].setWorldY(y * gP.getTamanioTile());
+						int x,y,tilen;
+						do { //esto es para que los objetos no salgan en espacios donde el perosnaje no puede pasar por colisión
+						x = rand.nextInt(50); // suponiendo 50 tiles en X
+						y = rand.nextInt(30); // suponiendo 30 tiles en Y
+						
+						tilen = gP.mTi.getCodigoMapaTiles(y,x);
+						
+						}while(gP.mTi.getColisionDeTile(tilen) == true);
+						
+						gP.o[i].setWorldX(x * gP.getTamanioTile());
+						gP.o[i].setWorldY(y * gP.getTamanioTile());
 		}
 	}
 
